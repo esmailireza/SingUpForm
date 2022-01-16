ebbd54176095ace26c3ef4c73fa718145af24fde
esmailireza <esmailireza20@gmail.com>
2022-01-16 17:42:52 +0330

    defind states and set changeHandler to inputs

--- src/components/SingUpForm.js
+++ src/components/SingUpForm.js
@@ -1,18 +1,44 @@
+import { useState } from "react";
+
 const SingUpForm = () => {
+  const [userData, setUserData] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+
+  const changeHandler = ({ target }) => {
+    setUserData({ ...userData, [target.name]: target.value });
+  };
   return (
     <div>
       <form>
         <div className="formControl">
           <label>Name</label>
-          <input type="text" />
+          <input
+            type="text"
+            onChange={changeHandler}
+            name="name"
+            value={userData.name}
+          />
         </div>
         <div className="formControl">
           <label>Email</label>
-          <input type="text" />
+          <input
+            type="text"
+            onChange={changeHandler}
+            name="email"
+            value={userData.email}
+          />
         </div>
         <div className="formControl">
           <label>Password</label>
-          <input type="text" />
+          <input
+            type="text"
+            onChange={changeHandler}
+            name="password"
+            value={userData.password}
+          />
         </div>
         <button>Submit</button>
       </form>
